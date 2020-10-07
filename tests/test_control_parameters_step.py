a859ba3f5885f67d866374a68fd28b87cea0fc61
Paul Saxe <paul@thesaxes.net>
2020-10-07 09:17:30 -0400

    Added a simple test so it doesn't fail.

--- tests/test_control_parameters_step.py
+++ tests/test_control_parameters_step.py
@@ -5,3 +5,11 @@
 
 import pytest  # noqa: F401
 import control_parameters_step  # noqa: F401
+
+
+def test_construction():
+    """Just create an object and test its type."""
+    result = control_parameters_step.ControlParameters()
+    assert str(type(result)) == (
+        "<class 'control_parameters_step.control_parameters.ControlParameters'>"  # noqa: E501
+    )
